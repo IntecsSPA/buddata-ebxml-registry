4fede6736d534cb4fd6fd018d85906bc16ef3dcd
cryptosense <cryptosense@92459c84-4455-0410-85d1-49e9ea8c5a13>
2009-01-15 09:40:19 +0000

    add @XmlRootElement annotation to GetRecordsResponseType

--- trunk/ErgoRR-jaxb/src/be/kzen/ergorr/model/csw/GetRecordsResponseType.java
+++ trunk/ErgoRR-jaxb/src/be/kzen/ergorr/model/csw/GetRecordsResponseType.java
@@ -5,6 +5,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
@@ -43,6 +44,7 @@ import javax.xml.bind.annotation.XmlType;
     "searchStatus",
     "searchResults"
 })
+@XmlRootElement
 public class GetRecordsResponseType {
 
     @XmlElement(name = "RequestId")
