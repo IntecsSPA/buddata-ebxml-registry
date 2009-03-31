9ed78eb48f0dfd25289722a4054a8bfb5ad8d710
fanciulli <fanciulli@92459c84-4455-0410-85d1-49e9ea8c5a13>
2009-03-31 15:23:20 +0000

    adding code

---
+++ trunk/ErgoRR/ErgoRR-backend/src/main/java/be/kzen/ergorr/service/TransactionService.java
@@ -0,0 +1,270 @@
+/*
+ * Project: Buddata ebXML RegRep
+ * Class: TransactionService.java
+ * Copyright (C) 2008 Yaman Ustuntas
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package be.kzen.ergorr.service;
+
+import be.kzen.ergorr.commons.CommonFunctions;
+import be.kzen.ergorr.commons.NamespaceConstants;
+import be.kzen.ergorr.exceptions.TranslationException;
+import be.kzen.ergorr.commons.RequestContext;
+import be.kzen.ergorr.interfaces.soap.ServiceExceptionReport;
+import be.kzen.ergorr.model.csw.BriefRecordType;
+import be.kzen.ergorr.model.csw.DeleteType;
+import be.kzen.ergorr.model.csw.ElementSetNameType;
+import be.kzen.ergorr.model.csw.ElementSetType;
+import be.kzen.ergorr.model.csw.GetRecordsResponseType;
+import be.kzen.ergorr.model.csw.GetRecordsType;
+import be.kzen.ergorr.model.csw.InsertResultType;
+import be.kzen.ergorr.model.csw.InsertType;
+import be.kzen.ergorr.model.csw.QueryType;
+import be.kzen.ergorr.model.csw.TransactionResponseType;
+import be.kzen.ergorr.model.csw.TransactionSummaryType;
+import be.kzen.ergorr.model.csw.TransactionType;
+import be.kzen.ergorr.model.csw.UpdateType;
+import be.kzen.ergorr.model.purl.elements.SimpleLiteral;
+import be.kzen.ergorr.model.rim.IdentifiableType;
+import be.kzen.ergorr.model.rim.RegistryObjectListType;
+import be.kzen.ergorr.model.rim.RegistryObjectType;
+import be.kzen.ergorr.model.util.OFactory;
+import be.kzen.ergorr.query.QueryManager;
+import be.kzen.ergorr.service.translator.TranslationFactory;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+/**
+ *
+ * @author Yaman Ustuntas
+ */
+public class TransactionService {
+
+    private static Logger logger = Logger.getLogger(TransactionService.class.getName());
+    private RequestContext requestContext;
+    private InsertResultType insertResult;
+    private int inserts;
+    private int updates;
+    private int deletes;
+
+    public TransactionService() {
+        insertResult = new InsertResultType();
+        inserts = 0;
+        updates = 0;
+        deletes = 0;
+    }
+
+    public TransactionService(RequestContext requestContext) {
+        this();
+        this.requestContext = requestContext;
+    }
+
+    /**
+     * Start processing the transaction request.
+     *
+     * @return Transaction response.
+     * @throws be.kzen.ergorr.interfaces.soap.ServiceExceptionReport
+     */
+    public TransactionResponseType process() throws ServiceExceptionReport {
+        List<Object> iuds = ((TransactionType) requestContext.getRequest()).getInsertOrUpdateOrDelete();
+
+        for (Object iud : iuds) {
+            if (iud instanceof InsertType) {
+                doInsert((InsertType) iud);
+            } else if (iud instanceof UpdateType) {
+                doUpdate((UpdateType) iud);
+            } else if (iud instanceof DeleteType) {
+                doDelete((DeleteType) iud);
+            } else {
+                logger.severe("Transaction request not an Insert, Update or Delete");
+            }
+        }
+
+        return buildResponse();
+    }
+
+    /**
+     * Builds the transaction response from data collection from insert/update/delete.
+     *
+     * @return Transaction response.
+     */
+    private TransactionResponseType buildResponse() {
+        TransactionResponseType response = new TransactionResponseType();
+        TransactionSummaryType summary = new TransactionSummaryType();
+
+        if (!insertResult.getBriefRecord().isEmpty()) {
+            response.getInsertResult().add(insertResult);
+        }
+        if (inserts > 0) {
+            summary.setTotalInserted(BigInteger.valueOf(inserts));
+        }
+        if (updates > 0) {
+            summary.setTotalUpdated(BigInteger.valueOf(updates));
+        }
+        if (deletes > 0) {
+            summary.setTotalDeleted(BigInteger.valueOf(deletes));
+        }
+
+        response.setTransactionSummary(summary);
+        return response;
+    }
+
+    /**
+     * Handles the Insert transaction.
+     *
+     * @param insert Insert data.
+     * @throws be.kzen.ergorr.interfaces.soap.ServiceExceptionReport
+     */
+    private void doInsert(InsertType insert) throws ServiceExceptionReport {
+        logger.fine("Transtaction.doInsert");
+        TranslationFactory transFac = new TranslationFactory();
+        RegistryObjectListType regObjList = new RegistryObjectListType();
+
+        try {
+            for (Object o : insert.getAny()) {
+                Object obj = ((JAXBElement) o).getValue();
+
+                if (logger.isLoggable(Level.FINE)) {
+                    logger.fine("Translation root object: " + obj.getClass().toString());
+                }
+                if (obj instanceof IdentifiableType) {
+                    regObjList.getIdentifiable().add((JAXBElement<? extends IdentifiableType>) o);
+                } else if (obj instanceof RegistryObjectListType) {
+                    RegistryObjectListType reqList = (RegistryObjectListType) obj;
+                    regObjList.getIdentifiable().addAll(reqList.getIdentifiable());
+                } else {
+                    regObjList.getIdentifiable().addAll(transFac.translate(obj).getIdentifiable());
+                }
+            }
+
+            if (logger.isLoggable(Level.FINE)) {
+                logger.fine("Got " + regObjList.getIdentifiable().size() + " translated objects");
+            }
+            LCManager lcm = new LCManager(requestContext);
+            lcm.submit(regObjList);
+
+            inserts += regObjList.getIdentifiable().size();
+            appendBriefRecords(regObjList);
+        } catch (TranslationException ex) {
+            logger.log(Level.SEVERE, "Transaction error", ex);
+            throw new ServiceExceptionReport("Transaction error", null, ex);
+        }
+    }
+
+    private void doUpdate(UpdateType update) throws ServiceExceptionReport {
+
+        if (update.isSetAny() && update.getAny() instanceof JAXBElement) {
+            Object updateObj = ((JAXBElement) update.getAny()).getValue();
+
+            RegistryObjectListType regObjList = null;
+
+            if (updateObj instanceof RegistryObjectListType) {
+                regObjList = (RegistryObjectListType) updateObj;
+            } else if (updateObj instanceof RegistryObjectType) {
+                regObjList = new RegistryObjectListType();
+                JAXBElement<RegistryObjectType> regObjEl = OFactory.rim.createRegistryObject((RegistryObjectType) updateObj);
+                regObjList.getIdentifiable().add(regObjEl);
+            } else {
+                throw new ServiceExceptionReport("Expected RegistryObjectList or any RegistryObject sub type");
+            }
+
+            LCManager lcm = new LCManager(requestContext);
+            lcm.update(regObjList);
+
+            int updateCount = 0;
+            int insertCount = 0;
+
+            for (JAXBElement<? extends IdentifiableType> identEl : regObjList.getIdentifiable()) {
+                IdentifiableType ident = identEl.getValue();
+
+                if (ident.isNewObject()) {
+                    insertCount++;
+                    appendBriefRecord(ident);
+                } else {
+                    updateCount++;
+                }
+            }
+
+            inserts += insertCount;
+            updates += updateCount;
+        } else {
+            throw new ServiceExceptionReport("No rim:RegistryObject or rim:RegistryObjectList specified for updating");
+        }
+    }
+
+    private void doDelete(DeleteType delete) throws ServiceExceptionReport {
+        GetRecordsType getRecords = new GetRecordsType();
+        QueryType query = new QueryType();
+        String typeName = CommonFunctions.removePrefix(delete.getTypeName());
+        QName objQNameToDelete = new QName(NamespaceConstants.RIM, typeName, "rim");
+        query.getTypeNames().add(objQNameToDelete);
+
+        ElementSetNameType elSetName = new ElementSetNameType();
+        elSetName.getTypeNames().add(objQNameToDelete);
+        elSetName.setValue(ElementSetType.BRIEF);
+        query.setElementSetName(elSetName);
+
+        query.setConstraint(delete.getConstraint());
+        JAXBElement<QueryType> queryEl = OFactory.csw.createQuery(query);
+        getRecords.setAbstractQuery(queryEl);
+
+        RequestContext rq = new RequestContext();
+        rq.setRequest(getRecords);
+
+        QueryManager qm = new QueryManager(rq);
+
+        GetRecordsResponseType queryResponse = qm.query();
+        List<Object> objsToDelete = queryResponse.getSearchResults().getAny();
+
+        RegistryObjectListType regObjList = new RegistryObjectListType();
+
+        for (Object objToDelete : objsToDelete) {
+            JAXBElement<? extends IdentifiableType> elToDelete = (JAXBElement<? extends IdentifiableType>) objToDelete;
+            regObjList.getIdentifiable().add(elToDelete);
+        }
+
+        LCManager lcm = new LCManager(requestContext);
+        lcm.delete(regObjList);
+
+        deletes = regObjList.getIdentifiable().size();
+    }
+
+    private void appendBriefRecord(IdentifiableType ident) {
+        BriefRecordType briefRecord = new BriefRecordType();
+
+        SimpleLiteral identifier = new SimpleLiteral();
+        identifier.getContent().add(ident.getId());
+        briefRecord.getIdentifier().add(OFactory.purl_elements.createIdentifier(identifier));
+
+        if (ident instanceof RegistryObjectType) {
+            SimpleLiteral type = new SimpleLiteral();
+            type.getContent().add(((RegistryObjectType) ident).getObjectType());
+            briefRecord.setType(type);
+            insertResult.getBriefRecord().add(briefRecord);
+        }
+
+    }
+
+    private void appendBriefRecords(RegistryObjectListType regObjList) {
+        for (JAXBElement<? extends IdentifiableType> identEl : regObjList.getIdentifiable()) {
+            appendBriefRecord(identEl.getValue());
+        }
+    }
+}
---
+++ trunk/ErgoRR/ErgoRR-backend/src/main/java/be/kzen/ergorr/service/translator/eo/PHRTranslator.java
@@ -0,0 +1,42 @@
+/*
+ * Project: Buddata ebXML RegRep
+ * Class: PHRTranslator.java
+ * Copyright (C) 2008 Yaman Ustuntas
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package be.kzen.ergorr.service.translator.eo;
+
+import be.kzen.ergorr.model.eo.opt.EarthObservationType;
+import be.kzen.ergorr.service.translator.Translator;
+
+/**
+ * TO BE IMPLEMENTED
+ * 
+ * @author Yaman Ustuntas
+ */
+public class PHRTranslator extends OPTTranslator implements Translator {
+    private EarthObservationType eo;
+    private static final String CLASSIFICATION = "urn:x-ogc:specification:csw-ebrim:EO:EOProductTypes:SAR"; 
+
+    public PHRTranslator(EarthObservationType eo) {
+        super(eo);
+        this.eo = eo;
+    }
+    
+    @Override
+    public String getClassification() {
+        return CLASSIFICATION;
+    }
+}
