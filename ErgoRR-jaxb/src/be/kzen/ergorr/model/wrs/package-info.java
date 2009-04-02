41f64b10b34462b21b1327440d26c650eb5b79a7
cryptosense <cryptosense@92459c84-4455-0410-85d1-49e9ea8c5a13>
2009-04-02 15:45:16 +0000

    remove old projects

--- ErgoRR-jaxb/src/be/kzen/ergorr/model/wrs/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/cat/wrs/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package be.kzen.ergorr.model.wrs;
