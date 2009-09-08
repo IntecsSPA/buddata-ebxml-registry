aba71039ab63d8d18e8dd1e7c4f184f50ddfe15c
cryptosense <cryptosense@92459c84-4455-0410-85d1-49e9ea8c5a13>
2009-09-08 09:37:56 +0000

    updates

--- trunk/ErgoRR/ErgoRR-persistence/src/main/java/be/kzen/ergorr/persist/SyntaxElements.java
+++ trunk/ErgoRR/ErgoRR-persistence/src/main/java/be/kzen/ergorr/persist/SyntaxElements.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package be.kzen.ergorr.persist;
 
 import be.kzen.ergorr.commons.CommonFunctions;
