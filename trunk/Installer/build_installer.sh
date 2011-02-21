050802d8cb1f63eaa5074aef22e7bbf684e79661
rbadnav <rbadnav@92459c84-4455-0410-85d1-49e9ea8c5a13>
2011-02-21 10:15:05 +0000

    build_installer.sh now executes a maven build and copies the generated binaries prior to running.

--- trunk/Installer/build_installer.sh
+++ trunk/Installer/build_installer.sh
@@ -1,1 +1,12 @@
+cd ../ErgoRR
+mvn clean install
+
+cp ErgoRR-backend/target/ErgoRR-backend-1.0-SNAPSHOT.jar ../Installer/distribution/
+cp ErgoRR-backend-client/target/ErgoRR-backend-client-1.0-SNAPSHOT.jar ../Installer/distribution/
+cp ErgoRR-client/target/ErgoRR-client-1.0-SNAPSHOT.jar ../Installer/distribution/
+cp ErgoRR-commons/target/ErgoRR-commons-1.0-SNAPSHOT.jar ../Installer/distribution/
+cp ErgoRR-jaxb/target/ErgoRR-jaxb-1.0-SNAPSHOT.jar ../Installer/distribution/
+cp ErgoRR-persistence/target/ErgoRR-persistence-1.0-SNAPSHOT.jar ../Installer/distribution/
+
+cd ../Installer
 compile ergo_install.xml -b . -o ErgoRR-installer.jar -k standard
