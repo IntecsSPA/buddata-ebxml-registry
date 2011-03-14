863994f6b2c5e868c643a0c52fffe27417ae5bb1
andreamarongiu@gmail.com <andreamarongiu@gmail.com@92459c84-4455-0410-85d1-49e9ea8c5a13>
2011-03-14 16:08:04 +0000

    XmlInterface ErgoRR GUI

--- branches/ConfigUI/ErgoRR/ErgoRR-web/src/main/java/it/intecs/pisa/erogorr/ui/GetResource.java
+++ branches/ConfigUI/ErgoRR/ErgoRR-web/src/main/java/it/intecs/pisa/erogorr/ui/GetResource.java
@@ -10,14 +10,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import sun.misc.BASE64Decoder;
 
 /**
  *
@@ -25,11 +21,12 @@ import sun.misc.BASE64Decoder;
  */
 public class GetResource extends HttpServlet {
 
-    protected static final String REST_ADMIN_PANELS = "/panels/admin";
-    protected static final String REST_USER_PANELS = "/panels/user";
+    protected static final String REST_RESOURCE_PANELS = "/resources/panels";
+    //protected static final String REST_USER_PANELS = "/panels/user";
     protected static final String USER_PANELS_RESOURCE = "userPanels.js";
     protected static final String ADMIN_PANELS_RESOURCE = "adminPanels.js";
     protected static final String WEBINF_RESOURCES_FOLDER = "WEB-INF/conf/resources/";
+    
 
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -45,15 +42,20 @@ public class GetResource extends HttpServlet {
         File webInfResourceFolder=new File(getServletContext().getRealPath(WEBINF_RESOURCES_FOLDER));
         OutputStream out = response.getOutputStream();
         try {
-            authenticateUser(request);
             uri = request.getRequestURI();
-            if (uri.endsWith(REST_ADMIN_PANELS)) 
-                copy(new FileInputStream(new File(webInfResourceFolder, ADMIN_PANELS_RESOURCE)), out);
-             else 
-                if (uri.endsWith(REST_USER_PANELS))
-                 copy(new FileInputStream(new File(webInfResourceFolder, USER_PANELS_RESOURCE)), out);
-        } finally {
+            if (uri.endsWith(REST_RESOURCE_PANELS)) {
+                if(authenticate(request, "admin"))
+                    copy(new FileInputStream(new File(webInfResourceFolder, ADMIN_PANELS_RESOURCE)), out);
+                else 
+                if (authenticate(request, "user"))
+                    copy(new FileInputStream(new File(webInfResourceFolder, USER_PANELS_RESOURCE)), out);
+                
+            }    
+        }catch (Exception ex){
             request.getSession().invalidate();
+            response.sendError(401);
+        } finally {
+            
             out.close();
         }
     }
@@ -98,44 +100,17 @@ public class GetResource extends HttpServlet {
     }// </editor-fold>
 
 
-    /**
-     * This method checks client authorization.
-     * @param request Request class containing all information to use to authenticate
-     * the user
-     * @return True or False depending of the user authorization.
-     */
-    private boolean authenticateUser(HttpServletRequest request) throws IOException {
-        BASE64Decoder decoder = new BASE64Decoder();
-        String userID,password;
-        boolean valid=false;
-        StringTokenizer st = new java.util.StringTokenizer(request.getHeader("Authorization"));
+    
 
-        if (st.hasMoreTokens()) {
-            String basic = st.nextToken();
-            // We only handle HTTP Basic authentication
-
-            if (basic.equalsIgnoreCase("Basic")) {
-               String credentials = st.nextToken();
-               decoder = new sun.misc.BASE64Decoder();
-               String userPass =
-                  new String(decoder.decodeBuffer(credentials));
-
-               int p = userPass.indexOf(":");
-               if (p != -1) {
-                  userID = userPass.substring(0, p);
-                  password = userPass.substring(p+1);     
-
-                  if ((!userID.trim().equals("")) &&
-                      (!password.trim().equals(""))) {
-                     valid = true;
-                  }
-               }
-            }
-         }
-
-        return valid;
+    private boolean authenticate(HttpServletRequest request, String role) throws Exception{
+       AuthenticationManager am= new AuthenticationManager();
+       String [] roles= am.authenticateUser(request);
+       for(int i=0; i<roles.length; i++)
+           if(roles[i].equals(role))
+              return true;
+       return false;
     }
-
+    
     private static void copy(InputStream in, OutputStream out) throws IOException {
         synchronized (in) {
             synchronized (out) {
