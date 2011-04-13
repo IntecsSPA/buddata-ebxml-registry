0eb6922ff91e0153142276624f98d4273a8c01d5
twimpiex@gnx.be <twimpiex@gnx.be@92459c84-4455-0410-85d1-49e9ea8c5a13>
2011-04-13 11:28:21 +0000

    buidl scripts now also build custom elements

--- Installer/build_installer.sh
+++ Installer/build_installer.sh
@@ -1,12 +1,17 @@
 cd ../ErgoRR
 mvn clean install
-
 cp ErgoRR-backend/target/ErgoRR-backend-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-backend-client/target/ErgoRR-backend-client-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-client/target/ErgoRR-client-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-commons/target/ErgoRR-commons-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-jaxb/target/ErgoRR-jaxb-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-persistence/target/ErgoRR-persistence-1.0-SNAPSHOT.jar ../Installer/distribution/
+cd -
+
+cd CustomElements
+mvn clean install
+cp target/CustomIzPackElements-1.0-SNAPSHOT.jar ../distribution/
+cd -
 
 cd ../Installer
 $IZPACK_HOME/bin/compile ergo_install.xml -b . -o ErgoRR-installer.jar -k standard
