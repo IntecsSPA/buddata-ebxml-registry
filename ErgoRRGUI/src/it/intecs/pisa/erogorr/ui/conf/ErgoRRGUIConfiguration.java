4ba66a89a31e9733a8e95ebf904fbcea8def0492
andreamarongiu@gmail.com <andreamarongiu@gmail.com@92459c84-4455-0410-85d1-49e9ea8c5a13>
2011-04-21 17:06:29 +0000

    catalogue and GUI under Proxy: bug fixed

--- ErgoRRGUI/src/it/intecs/pisa/erogorr/ui/conf/ErgoRRGUIConfiguration.java
+++ ErgoRRGUI/src/it/intecs/pisa/erogorr/ui/conf/ErgoRRGUIConfiguration.java
@@ -21,8 +21,11 @@ public class ErgoRRGUIConfiguration {
     
     public static final String ERGORR_URL_PROPERTY = "ergoRRURL";
     public static final String ERGORR_LOCAL_URL_PROPERTY = "ergoRRLocalURL";
-    public static final String HOST_LOCAL_URL = "hostLocalURL";
     public static final String LOCALHOST_PROPERTY = "checkLocaHost";
+    
+    public static final String HOST_PROPERTY = "host";
+    public static final String PORT_PROPERTY = "property";
+
     
     private Hashtable configValues;
     
@@ -60,6 +63,8 @@ public class ErgoRRGUIConfiguration {
     public void addLocalAddressProperty(String ip, String port) throws Exception{
         String externalErgoRRURL=(String) this.configValues.get(ERGORR_URL_PROPERTY);
         String [] urlSplit=null;
+        NodeList propNodes=null;
+        String propName;
         Element newProp=null;
         DOMUtil domUtil=new DOMUtil();
         Document confDocument=domUtil.fileToDocument(this.configurationFile);
@@ -67,16 +72,35 @@ public class ErgoRRGUIConfiguration {
         String temp=externalErgoRRURL.replaceAll("http://", "");
         urlSplit=temp.split("/");
         String internalErgoRRURL=externalErgoRRURL.replaceAll(urlSplit[0], ip+":"+port);
+        propNodes=confDocument.getDocumentElement().getElementsByTagName(PROPERTY_ELEMENT);
         
-        newProp=confDocument.createElement(PROPERTY_ELEMENT);
-        newProp.setAttribute(PROPERTY_NAME_ATTRIBUTE, HOST_LOCAL_URL);
-        newProp.setAttribute(PROPERTY_VALUE_ATTRIBUTE, "http://"+ip+":"+port);
-        root.appendChild(newProp);
-        
-        newProp=confDocument.createElement(PROPERTY_ELEMENT);
-        newProp.setAttribute(PROPERTY_NAME_ATTRIBUTE, ERGORR_LOCAL_URL_PROPERTY);
-        newProp.setAttribute(PROPERTY_VALUE_ATTRIBUTE, internalErgoRRURL);
-        root.appendChild(newProp);
+        if(propNodes.getLength()==1){
+            
+            newProp=confDocument.createElement(PROPERTY_ELEMENT);
+            newProp.setAttribute(PROPERTY_NAME_ATTRIBUTE, HOST_PROPERTY);
+            newProp.setAttribute(PROPERTY_VALUE_ATTRIBUTE, ip);
+            root.appendChild(newProp);
+            
+            newProp=confDocument.createElement(PROPERTY_ELEMENT);
+            newProp.setAttribute(PROPERTY_NAME_ATTRIBUTE, PORT_PROPERTY);
+            newProp.setAttribute(PROPERTY_VALUE_ATTRIBUTE, port);
+            root.appendChild(newProp);
+
+            newProp=confDocument.createElement(PROPERTY_ELEMENT);
+            newProp.setAttribute(PROPERTY_NAME_ATTRIBUTE, ERGORR_LOCAL_URL_PROPERTY);
+            newProp.setAttribute(PROPERTY_VALUE_ATTRIBUTE, internalErgoRRURL);
+            root.appendChild(newProp);
+        }else{
+            for(int i=0;i<propNodes.getLength();i++){
+                propName=propNodes.item(i).getAttributes().getNamedItem(PROPERTY_NAME_ATTRIBUTE).getNodeValue(); 
+                if(propName.equals(HOST_PROPERTY))
+                   propNodes.item(i).getAttributes().getNamedItem(PROPERTY_VALUE_ATTRIBUTE).setNodeValue(ip);   
+                if(propName.equals(PORT_PROPERTY))
+                   propNodes.item(i).getAttributes().getNamedItem(PROPERTY_VALUE_ATTRIBUTE).setNodeValue(port);   
+                if(propName.equals(ERGORR_LOCAL_URL_PROPERTY))
+                   propNodes.item(i).getAttributes().getNamedItem(PROPERTY_VALUE_ATTRIBUTE).setNodeValue(internalErgoRRURL);
+            }
+        }    
         
         this.loadProperties();
         
