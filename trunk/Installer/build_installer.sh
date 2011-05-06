6a1719daae274766fb19d67650e51bc98867a923
twimpiex@gnx.be <twimpiex@gnx.be@92459c84-4455-0410-85d1-49e9ea8c5a13>
2011-05-06 07:33:16 +0000

    updated isntaller script and bumped to version 2.2.2

--- trunk/Installer/build_installer.sh
+++ trunk/Installer/build_installer.sh
@@ -1,4 +1,8 @@
+export APPVERSION=`sed '/^\#/d' ergo_install.xml | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | grep appversion | tail -n 1 | cut -d ">" -f2- | cut -d "<" -f1 | cut -d " " -f2`
+
 cd ../ErgoRR
+cp conf/ergorr-template.properties conf/ergorr.properties 
+export DEPLOY_NAME=`sed '/^\#/d' conf/ergorr.properties | grep 'deployName'  | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
 mvn clean install
 cp ErgoRR-backend/target/ErgoRR-backend-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-backend-client/target/ErgoRR-backend-client-1.0-SNAPSHOT.jar ../Installer/distribution/
@@ -6,7 +10,9 @@ cp ErgoRR-client/target/ErgoRR-client-1.0-SNAPSHOT.jar ../Installer/distribution
 cp ErgoRR-commons/target/ErgoRR-commons-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-jaxb/target/ErgoRR-jaxb-1.0-SNAPSHOT.jar ../Installer/distribution/
 cp ErgoRR-persistence/target/ErgoRR-persistence-1.0-SNAPSHOT.jar ../Installer/distribution/
-cp -Rf ErgoRR-web/target/$DEFAULT_ERGORR_WEB_NAME/* ../Installer/distribution/unpacked-war
+rm -rf ../Installer/distribution/unpacked-war/
+cp -a ErgoRR-web/target/${DEPLOY_NAME}/ ../Installer/distribution/unpacked-war
+rm ../Installer/distribution/unpacked-war/WEB-INF/classes/ergorr.properties
 cd -
 
 # ErgoRR GUI -- START
@@ -22,4 +28,4 @@ cp target/CustomIzPackElements-1.0-SNAPSHOT.jar ../distribution/
 cd -
 
 cd ../Installer
-$IZPACK_HOME/bin/compile ergo_install.xml -b . -o ErgoRR-installer.jar -k standard
+$IZPACK_HOME/bin/compile ergo_install.xml -b . -o ErgoRR-installeri_${APPVERSION}.jar -k standard
