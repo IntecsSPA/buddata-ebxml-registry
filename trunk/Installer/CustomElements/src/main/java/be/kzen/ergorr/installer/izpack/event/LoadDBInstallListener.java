23aec26a5a6f2067a1e7dd0c147143d1f0cd0f32
mariarosaria.7 <mariarosaria.7@92459c84-4455-0410-85d1-49e9ea8c5a13>
2011-05-11 08:41:47 +0000

    Added the SensorML Extension Package

--- trunk/Installer/CustomElements/src/main/java/be/kzen/ergorr/installer/izpack/event/LoadDBInstallListener.java
+++ trunk/Installer/CustomElements/src/main/java/be/kzen/ergorr/installer/izpack/event/LoadDBInstallListener.java
@@ -60,219 +60,217 @@ import com.izforge.izpack.util.AbstractUIProgressHandler;
  */
 public class LoadDBInstallListener extends SimpleInstallerListener {
 
-	private static final String[] DEFAULT_SQL_FILES = { "database.sql" };
+    private static final String[] DEFAULT_SQL_FILES = {"database.sql"};
+    private static final String[] DEFAULT_XML_FILES = {
+        "rim-objecttype-scheme.xml",
+        "rim-datatype-scheme.xml",
+        "rim-associationtype-scheme.xml",
+        "rim-querylanguage-scheme.xml",
+        "ergo.xml",
+        "OGC-root-package.xml",
+        "eo-rim-model.xml",
+        "eo-slot-init.xml",
+        "ISO19119-Services-Scheme.xml",
+        "UNSD-Regions-Scheme.xml",
+        "Basic-Package.xml",
+        "basic-package-slot-init.xml",
+        "CIM-CharacterSetScheme.xml",
+        "CIM-CitedResponsiblePartyScheme.xml",
+        "CIM-ClassificationCodeScheme.xml",
+        "CIM-CouplingTypeScheme.xml",
+        "CIM-DCPListScheme.xml",
+        "CIM-FormatNameAndVersionScheme.xml",
+        "CIM-KeywordScheme.xml",
+        "CIM-KeywordTypeScheme.xml",
+        "CIM-MetadataStandardNameAndVersionScheme.xml",
+        "CIM-RestrictionCodeScheme.xml",
+        "CIM-RestrictionTypeScheme.xml",
+        "CIM-SpatialRepresentationScheme.xml",
+        "CIM-TopicCategoryScheme.xml",
+        "CIM-Package.xml",
+        "cim-package-slot-init.xml",
+        "SensorML-IntendedApplicationScheme.xml",
+        "SensorML-OrbitTypeScheme.xml",
+        "SensorML-SystemTypeScheme.xml",
+        "SensorML-Package.xml",
+        "SensorML-Package-slot-init.xml"};
+    private String dbUrl;
+    private String dbName;
+    private String dbUser;
+    private String dbPassword;
+    private String dataFileString;
+    private boolean writeDefaults;
+    private String[] dataFiles;
 
-	private static final String[] DEFAULT_XML_FILES = {
-			"rim-objecttype-scheme.xml",
-                        "rim-datatype-scheme.xml",
-			"rim-associationtype-scheme.xml",
-                        "rim-querylanguage-scheme.xml",
-			"ergo.xml", 
-                        "OGC-root-package.xml",
-                        "eo-rim-model.xml",
-			"eo-slot-init.xml",
-                        "ISO19119-Services-Scheme.xml",
-			"UNSD-Regions-Scheme.xml",
-                        "Basic-Package.xml",
-			"basic-package-slot-init.xml",
-                        "CIM-CharacterSetScheme.xml",
-			"CIM-CitedResponsiblePartyScheme.xml",
-			"CIM-ClassificationCodeScheme.xml",
-                        "CIM-CouplingTypeScheme.xml",
-			"CIM-DCPListScheme.xml",
-                        "CIM-FormatNameAndVersionScheme.xml",
-			"CIM-KeywordScheme.xml",
-                        "CIM-KeywordTypeScheme.xml",
-			"CIM-MetadataStandardNameAndVersionScheme.xml",
-			"CIM-RestrictionCodeScheme.xml", 
-                        "CIM-RestrictionTypeScheme.xml",
-			"CIM-SpatialRepresentationScheme.xml",
-			"CIM-TopicCategoryScheme.xml", 
-                        "CIM-Package.xml",
-                        "cim-package-slot-init.xml" };
+    public void afterPacks(AutomatedInstallData data,
+            AbstractUIProgressHandler handler) throws Exception {
+        super.afterPacks(data, handler);
 
-	private String dbUrl;
-	private String dbName;
-	private String dbUser;
-	private String dbPassword;
-	private String dataFileString;
-	private boolean writeDefaults;
+        install(data);
+    }
 
-	private String[] dataFiles;
+    private void install(AutomatedInstallData data) throws SQLException,
+            IOException {
+        mapParams(data);
+        fixErgorrProperties(data);
 
-	public void afterPacks(AutomatedInstallData data,
-			AbstractUIProgressHandler handler) throws Exception {
-		super.afterPacks(data, handler);
+        if (writeDefaults) {
+            File defaultRoot = getDefaultModelDirectory(data);
+            loadBaseModel(defaultRoot);
+        }
 
-		install(data);
-	}
+        if (dataFiles != null && dataFiles.length > 0) {
+            // Insert user specified XMl files
+            File[] files = new File[dataFiles.length];
+            for (int i = 0; i < dataFiles.length; i++) {
+                files[i] = new File(dataFiles[i]);
+            }
+            invoke(files);
+        }
+    }
 
-	private void install(AutomatedInstallData data) throws SQLException,
-			IOException {
-		mapParams(data);
-		fixErgorrProperties(data);
+    /**
+     * Create the database tables and insert the basic model.
+     */
+    private void loadBaseModel(File defaultRoot) throws SQLException,
+            IOException {
+        insertSQLData(defaultRoot);
+        File[] files = getModelFiles(defaultRoot, DEFAULT_XML_FILES);
+        invoke(files);
+    }
 
-		if (writeDefaults) {
-			File defaultRoot = getDefaultModelDirectory(data);
-			loadBaseModel(defaultRoot);
-		}
+    private void insertSQLData(File defaultRoot) throws SQLException,
+            IOException {
+        Database db = new Database(dbUrl, dbName, dbUser, dbPassword);
+        for (File f : getModelFiles(defaultRoot, DEFAULT_SQL_FILES)) {
+            db.executeSql(readFileAsString(f));
+        }
+    }
 
-		if (dataFiles != null && dataFiles.length > 0) {
-			// Insert user specified XMl files
-			File[] files = new File[dataFiles.length];
-			for (int i = 0; i < dataFiles.length; i++) {
-				files[i] = new File(dataFiles[i]);
-			}
-			invoke(files);
-		}
-	}
+    private void invoke(File[] xmlFiles) {
+        if (xmlFiles == null || xmlFiles.length == 0) {
+            return;
+        }
 
-	/**
-	 * Create the database tables and insert the basic model.
-	 */
-	private void loadBaseModel(File defaultRoot) throws SQLException,
-			IOException {
-		insertSQLData(defaultRoot);
-		File[] files = getModelFiles(defaultRoot, DEFAULT_XML_FILES);
-		invoke(files);
-	}
+        Unmarshaller unmarshaller = null;
 
-	private void insertSQLData(File defaultRoot) throws SQLException,
-			IOException {
-		Database db = new Database(dbUrl, dbName, dbUser, dbPassword);
-		for (File f : getModelFiles(defaultRoot, DEFAULT_SQL_FILES)) {
-			db.executeSql(readFileAsString(f));
-		}
-	}
+        try {
+            Thread.currentThread().setContextClassLoader(
+                    getClass().getClassLoader());
+            unmarshaller = JAXBUtil.getInstance().createUnmarshaller();
+        } catch (JAXBException ex) {
+            throw new RuntimeException("Could not create unmarshaller", ex);
+        }
 
-	private void invoke(File[] xmlFiles) {
-		if (xmlFiles == null || xmlFiles.length == 0) {
-			return;
-		}
+        for (File xmlFile : xmlFiles) {
+            TransactionType request = new TransactionType();
+            InsertType insert = new InsertType();
 
-		Unmarshaller unmarshaller = null;
+            try {
+                JAXBElement jaxbEl = (JAXBElement) unmarshaller.unmarshal(xmlFile);
+                insert.getAny().add(jaxbEl);
+                request.getInsertOrUpdateOrDelete().add(insert);
 
-		try {
-			Thread.currentThread().setContextClassLoader(
-					getClass().getClassLoader());
-			unmarshaller = JAXBUtil.getInstance().createUnmarshaller();
-		} catch (JAXBException ex) {
-			throw new RuntimeException("Could not create unmarshaller", ex);
-		}
+                RequestContext context = getRequestContext();
+                context.setRequest(request);
+                TransactionService service = new TransactionService(context);
+                TransactionResponseType response = service.process();
 
-		for (File xmlFile : xmlFiles) {
-			TransactionType request = new TransactionType();
-			InsertType insert = new InsertType();
+                String responseStr = JAXBUtil.getInstance().marshallToStr(
+                        OFactory.csw.createTransactionResponse(response));
+            } catch (ServiceException ex) {
+                ex.printStackTrace();
+            } catch (JAXBException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-			try {
-				JAXBElement jaxbEl = (JAXBElement) unmarshaller
-						.unmarshal(xmlFile);
-				insert.getAny().add(jaxbEl);
-				request.getInsertOrUpdateOrDelete().add(insert);
+    private void mapParams(AutomatedInstallData aid) {
+        dbUrl = aid.getVariable(Constants.URL_PARAM);
+        dbName = aid.getVariable(Constants.DB_PARAM);
+        dbUser = aid.getVariable(Constants.USER_PARAM);
+        dbPassword = aid.getVariable(Constants.PASS_PARAM);
+        dataFileString = aid.getVariable(Constants.DATAFILES_PARAM);
+        writeDefaults = Constants.TRUE.equals(aid.getVariable(Constants.WRITE_DB_PARAM));
 
-				RequestContext context = getRequestContext();
-				context.setRequest(request);
-				TransactionService service = new TransactionService(context);
-				TransactionResponseType response = service.process();
+        if (dataFileString != null) {
+            dataFiles = dataFileString.split(";");
+        }
+    }
 
-				String responseStr = JAXBUtil.getInstance().marshallToStr(
-						OFactory.csw.createTransactionResponse(response));
-			} catch (ServiceException ex) {
-				ex.printStackTrace();
-			} catch (JAXBException ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
+    private File getDefaultModelDirectory(AutomatedInstallData aid) {
+        File root = getInstallRoot(aid);
+        return new File(root, Constants.MODEL_DIR);
+    }
 
-	private void mapParams(AutomatedInstallData aid) {
-		dbUrl = aid.getVariable(Constants.URL_PARAM);
-		dbName = aid.getVariable(Constants.DB_PARAM);
-		dbUser = aid.getVariable(Constants.USER_PARAM);
-		dbPassword = aid.getVariable(Constants.PASS_PARAM);
-		dataFileString = aid.getVariable(Constants.DATAFILES_PARAM);
-		writeDefaults = Constants.TRUE.equals(aid
-				.getVariable(Constants.WRITE_DB_PARAM));
+    private boolean isStandalone(AutomatedInstallData aid) {
+        if (Constants.STANDALONE_TYPE.equals(aid.getVariable(Constants.INSTALLTYPE_PARAM))) {
+            return true;
+        }
+        return false;
+    }
 
-		if (dataFileString != null) {
-			dataFiles = dataFileString.split(";");
-		}
-	}
+    private File getInstallRoot(AutomatedInstallData aid) {
+        String path;
+        if (isStandalone(aid)) {
+            path = aid.getVariable(Constants.BACKEND_INSTALL_PATH_PARAM);
+        } else {
+            path = String.format(Constants.DEPLOY_PATH, aid.getVariable(Constants.TOMCAT_INSTALL_PATH_PARAM), dbName);
+        }
+        return new File(path);
+    }
 
-	private File getDefaultModelDirectory(AutomatedInstallData aid) {
-		File root = getInstallRoot(aid);
-		return new File(root, Constants.MODEL_DIR);
-	}
+    private File[] getModelFiles(File root, String[] filenames) {
+        File[] files = new File[filenames.length];
 
-	private boolean isStandalone(AutomatedInstallData aid) {
-		if (Constants.STANDALONE_TYPE.equals(aid
-				.getVariable(Constants.INSTALLTYPE_PARAM))) {
-			return true;
-		}
-		return false;
-	}
+        int i = 0;
+        for (String f : filenames) {
+            File file = new File(root, f);
+            files[i++] = file;
+        }
+        return files;
+    }
 
-	private File getInstallRoot(AutomatedInstallData aid) {
-		String path;
-		if (isStandalone(aid)) {
-			path = aid.getVariable(Constants.BACKEND_INSTALL_PATH_PARAM);
-		} else {
-			path = String.format(Constants.DEPLOY_PATH, aid
-					.getVariable(Constants.TOMCAT_INSTALL_PATH_PARAM), dbName);
-		}
-		return new File(path);
-	}
+    private RequestContext getRequestContext() {
+        RequestContext context = new RequestContext();
+        DbConnectionParams connexion = new DbConnectionParams(dbUrl, dbName,
+                dbUser, dbPassword);
+        context.putParam(InternalConstants.DB_CONNECTION_PARAMS, connexion);
+        return context;
+    }
 
-	private File[] getModelFiles(File root, String[] filenames) {
-		File[] files = new File[filenames.length];
+    private void fixErgorrProperties(AutomatedInstallData aid) {
+        File root = getInstallRoot(aid);
+        File ergorr;
+        if (isStandalone(aid)) {
+            ergorr = new File(root, Constants.ERGORR_PROPERTIES_FILE);
+        } else {
+            ergorr = new File(root, Constants.WEBAPP_CLASSPATH
+                    + Constants.ERGORR_PROPERTIES_FILE);
+        }
+        System.setProperty(Constants.ERGORR_PROPERTY, ergorr.getAbsolutePath());
+    }
 
-		int i = 0;
-		for (String f : filenames) {
-			File file = new File(root, f);
-			files[i++] = file;
-		}
-		return files;
-	}
+    /**
+     * Read a file into a String.
+     *
+     * @param file
+     *            File to read.
+     * @throws java.io.IOException
+     * @return File content as String.
+     */
+    private String readFileAsString(File file) throws IOException {
+        StringBuffer fileData = new StringBuffer();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        char[] buf = new char[2048];
+        int count = 0;
 
-	private RequestContext getRequestContext() {
-		RequestContext context = new RequestContext();
-		DbConnectionParams connexion = new DbConnectionParams(dbUrl, dbName,
-				dbUser, dbPassword);
-		context.putParam(InternalConstants.DB_CONNECTION_PARAMS, connexion);
-		return context;
-	}
-
-	private void fixErgorrProperties(AutomatedInstallData aid) {
-		File root = getInstallRoot(aid);
-		File ergorr;
-		if (isStandalone(aid)) {
-			ergorr = new File(root, Constants.ERGORR_PROPERTIES_FILE);
-		} else {
-			ergorr = new File(root, Constants.WEBAPP_CLASSPATH
-					+ Constants.ERGORR_PROPERTIES_FILE);
-		}
-		System.setProperty(Constants.ERGORR_PROPERTY, ergorr.getAbsolutePath());
-	}
-
-	/**
-	 * Read a file into a String.
-	 * 
-	 * @param file
-	 *            File to read.
-	 * @throws java.io.IOException
-	 * @return File content as String.
-	 */
-	private String readFileAsString(File file) throws IOException {
-		StringBuffer fileData = new StringBuffer();
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		char[] buf = new char[2048];
-		int count = 0;
-
-		while ((count = reader.read(buf)) > 0) {
-			fileData.append(String.valueOf(buf, 0, count));
-			buf = new char[1024];
-		}
-		reader.close();
-		return fileData.toString();
-	}
+        while ((count = reader.read(buf)) > 0) {
+            fileData.append(String.valueOf(buf, 0, count));
+            buf = new char[1024];
+        }
+        reader.close();
+        return fileData.toString();
+    }
 }
