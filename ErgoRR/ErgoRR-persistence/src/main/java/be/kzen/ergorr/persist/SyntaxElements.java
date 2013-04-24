dc23aa04cb86594168583ebcb8938c8ea7800e77
mariarosaria.7@gmail.com <mariarosaria.7@gmail.com@92459c84-4455-0410-85d1-49e9ea8c5a13>
2013-04-24 16:37:23 +0000

    Added the "DISTINCT" option to the "SELECT" clause to avoid duplicated results

--- ErgoRR/ErgoRR-persistence/src/main/java/be/kzen/ergorr/persist/SyntaxElements.java
+++ ErgoRR/ErgoRR-persistence/src/main/java/be/kzen/ergorr/persist/SyntaxElements.java
@@ -24,7 +24,8 @@ public class SyntaxElements {
     public static final String SPACE = " ";
     public static final String DOT = ".";
     public static final String COMMA = ",";
-    public static final String SELECT = " SELECT ";
+    // public static final String SELECT = " SELECT ";
+    public static final String SELECT = " SELECT DISTINCT ";
     public static final String WHERE = " WHERE ";
     public static final String FROM = " FROM ";
     public static final String LIKE = " LIKE ";
